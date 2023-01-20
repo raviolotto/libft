b601948b64924ae2c83ec61f91d4f65d849a89d8
Jacopo Cardinaux <jcardina@e3r3p8.42roma.it>
2023-01-20 14:04:54 +0100

    20.01.23 due

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,28 +6,35 @@
 /*   By: jcardina <jcardina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 10:44:24 by jcardina          #+#    #+#             */
-/*   Updated: 2023/01/20 10:44:35 by jcardina         ###   ########.fr       */
+/*   Updated: 2023/01/20 14:01:31 by jcardina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 #include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char		*tmp_dst;
-	unsigned char	*tmp_src;
+	unsigned char		*tmp_src;
 	size_t				i;
 
 	i = 0;
 	tmp_dst = (unsigned char *) dst;
 	tmp_src = (unsigned const char *) src;
-	if (src < dst)
+	if (dst < src)
 	{
 		while (i < len)
 		{
-			tmp_dst[i] = tmp_src[i];
+			((char *) dst)[i] = ((const char *) src)[i];
 			i++;
 		}
+	}
+	else
+	{
+		while (len--)
+			tmp_dst[len] = tmp_src[len];
 	}
 	return ((void *) dst);
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,7 +7,28 @@
 /*   By: jcardina <jcardina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 10:45:28 by jcardina          #+#    #+#             */
-/*   Updated: 2023/01/20 10:45:40 by jcardina         ###   ########.fr       */
+/*   Updated: 2023/01/20 12:33:21 by jcardina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t destsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dst_len;
+
+	i = 0;
+	dst_len = ft_strlen(dst);
+	j = dst_len + 1;
+	if (destsize == 0)
+		return (dst_len + (ft_strlen(src)));
+	while (i < (destsize - 1 - dst_len))
+	{
+		dst[j + i] = src[i];
+		i++;
+	}
+	dst[j + i] = 0;
+	return (dst_len + (ft_strlen(src)));
+}
